67e6ead981479870af34b31c506a1d696ba0098e
Albert Chang <albertchanged@gmail.com>
2018-01-16 19:42:13 -0800

    Refactor SignIn and NavBar to be Redux, progress on refactoring App

--- client/src/components/SignIn.jsx
+++ client/src/components/SignIn.jsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { Segment, Button, Divider, Icon, Message, Menu, Form, Header, Container, Card, Grid } from 'semantic-ui-react';
-
+import { connect } from 'react-redux';
+import { setUsername, setUserPassword, fetchUser } from '../js/actions/userActions.js';
+import { bindActionCreators } from 'redux';
+import store from '../js/store.js';
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      username: '',
-      password: ''
-    };
+    // this.state = {
+    //   username: '',
+    //   password: ''
+    // };
 
-    this.onChange = this.onChange.bind(this);
+    this.onUsernameChange = this.onUsernameChange.bind(this);
+    this.onPasswordChange = this.onPasswordChange.bind(this);
     this.onSignIn = this.onSignIn.bind(this);
   }
 
   onSignIn() {
-    this.props.onSignIn(this.state.username, this.state.password);
+    console.log('Signing in!');
+    let user = store.getState().user.user;
+    this.props.onSignIn(user.username, user.password);
   }
 
-  onChange(e, { value }) {
-    const name = e.target.name;
-    
-    this.setState({
-      [name]: value
-    });
+  onUsernameChange(e, { value }) {
+    // const name = e.target.name;
+    console.log(value);
+    // this.setState({
+    //   [name]: value
+    // });
+    this.props.setUsername(value);
+  }
+
+  onPasswordChange(e, { value }) {
+    // const name = e.target.name;
+    console.log(value);
+    // this.setState({
+    //   [name]: value
+    // });
+    this.props.setUserPassword(value);
   }
 
   render() {
@@ -35,8 +51,22 @@ class SignIn extends React.Component {
             <Header as='h1'>Sign In</Header>
             <Button className='githublogin' fluid href='/auth/github' color='black'><Icon name='github'/>Sign in with Github</Button>
             <Button className='googlelogin' fluid href='/auth/google' color='google plus' ><Icon name='google'/>Sign in with Google</Button>
-            <Form.Input label='username' name='username' value={this.state.username} onChange={this.onChange} autoComplete='username' placeholder='username' />
-            <Form.Input type='password' label='password' name='password' value={this.state.password} onChange={this.onChange} autoComplete='current-password' placeholder='password' />
+            <Form.Input 
+              label='username' 
+              name='username' 
+              // value={this.state.username} 
+              onChange={this.onUsernameChange} 
+              autoComplete='username' 
+              placeholder='username' 
+            />
+            <Form.Input 
+              type='password' 
+              label='password' 
+              name='password' 
+              // value={this.state.password} 
+              onChange={this.onPasswordChange} 
+              autoComplete='current-password' 
+              placeholder='password' />
             <Message
               error
               content={this.props.error}
@@ -49,4 +79,13 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = (state) => ({
+  username: state.user.username,
+  password: state.user.password
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ setUsername, setUserPassword, fetchUser }, dispatch);
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
