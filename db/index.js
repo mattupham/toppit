c656375187c36061cc7219cb1e03df6db3dc381d
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2018-01-17 20:07:58 -0800

    added viewedTopicList, rerender on new topic

--- db/index.js
+++ db/index.js
@@ -28,7 +28,7 @@ const commentSchema = mongoose.Schema({
 });
 
 const userSchema = mongoose.Schema({
-  _id:         mongoose.Schema.Types.ObjectId,
+  // _id:         mongoose.Schema.Types.ObjectId,
   username:    String,
   password:    String,
   googleId:    String,
